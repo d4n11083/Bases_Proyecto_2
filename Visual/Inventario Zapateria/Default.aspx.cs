8c5ff2b5182b30a9e0df28ede759e2d5d27c716e
Josué Chaves <d4n11083@estudiantec.cr>
2020-12-03 14:24:32 -0600

    Update Deafult: A la hora de buscar, reviza si el campo esta vacio y de ser necesario que sea un numero

--- Visual/Inventario Zapateria/Default.aspx.cs
+++ Visual/Inventario Zapateria/Default.aspx.cs
@@ -14,6 +14,41 @@ namespace Inventario_Zapateria
 
         }
 
+        private void Messagebox(string xMessage)
+        {
+            Response.Write("<script>alert('" + xMessage + "')</script>");
+        }
+
+        private bool checkField(TextBox textbox, bool checkIfNumber, string textBoxName)
+        {
+            bool isOk = true;
+            string message = " Debe ingresar un valor: ";
+
+            if (checkIfNumber)
+            {
+                int i;
+                if (!int.TryParse(textbox.Text, out i))
+                {
+                    message = " En este espacio solo se pueden colocar números: ";
+                    isOk = false;
+                }
+            }
+
+            if (textbox.Text == String.Empty)
+            {
+                isOk = false;
+                message += textBoxName;
+            }
+
+
+            if (!isOk)
+            {
+                Messagebox(message);
+            }
+
+            return isOk;
+        }
+
         private void searchBy(string data, string key)
         {
             LabelConsole.Text = " Buscar " + data + " por " + key;
@@ -21,33 +56,50 @@ namespace Inventario_Zapateria
 
         protected void btnSearchID_Click(object sender, EventArgs e)
         {
-            searchBy(TextBoxSearch.Text, btnSearchID.ID);
-
+            if (checkField(TextBoxSearch, true, "Busqueda por Id"))
+            {
+                searchBy(TextBoxSearch.Text, btnSearchID.ID);
+            }
         }
 
         protected void btnSearchModel_Click(object sender, EventArgs e)
         {
-            searchBy(TextBoxSearch.Text, btnSearchModel.ID);
+            if( checkField( TextBoxSearch, false, "Busqueda por Modelo" ))
+            {
+                searchBy(TextBoxSearch.Text, btnSearchModel.ID);
+            }
         }
 
         protected void btnSearchStyle_Click(object sender, EventArgs e)
         {
-            searchBy(TextBoxSearch.Text, btnSearchStyle.ID);
+            if( checkField( TextBoxSearch, false, "Busqueda por Estilo" ))
+            {
+                searchBy(TextBoxSearch.Text, btnSearchStyle.ID);
+            }
         }
 
         protected void btnSearchColour_Click(object sender, EventArgs e)
         {
-            searchBy(TextBoxSearch.Text, btnSearchColour.ID);
+            if( checkField( TextBoxSearch, false, "Busqueda por Color" ))
+            {
+                searchBy(TextBoxSearch.Text, btnSearchColour.ID);
+            }
         }
 
         protected void btnSearchSize_Click(object sender, EventArgs e)
         {
-            searchBy(TextBoxSearch.Text, btnSearchSize.ID);
+            if (checkField(TextBoxSearch, true, "Busqueda por Talla"))
+            {
+                searchBy(TextBoxSearch.Text, btnSearchSize.ID);
+            }
         }
 
         protected void btnSearchLine_Click(object sender, EventArgs e)
         {
-            searchBy(TextBoxSearch.Text, btnSearchLine.ID);
+            if (checkField(TextBoxSearch, false, "Busqueda por Linea"))
+            {
+                searchBy(TextBoxSearch.Text, btnSearchLine.ID);
+            }
         }
     }
 }

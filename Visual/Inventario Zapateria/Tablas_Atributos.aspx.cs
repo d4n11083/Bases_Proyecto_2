75c33601480169ace810a2c08934bce79aa387af
Josué Chaves <d4n11083@estudiantec.cr>
2020-12-03 13:59:57 -0600

    Update Tablas_Atributos: Ahora ya reviza los espacios, si esta vacio, muestra error y en el campo Talla solo permite numeros

--- Visual/Inventario Zapateria/Tablas_Atributos.aspx.cs
+++ Visual/Inventario Zapateria/Tablas_Atributos.aspx.cs
@@ -16,41 +16,85 @@ namespace Inventario_Zapateria
 
         }
 
+        private void Messagebox(string xMessage)
+        {
+            Response.Write("<script>alert('" + xMessage + "')</script>");
+        }
+
+        private bool checkField( TextBox textbox, bool checkIfNumber, string textBoxName)
+        {
+            bool isOk = true;
+            string message = " Debe ingresar un valor: ";
+
+            if( checkIfNumber)
+            {
+                int i;
+                if (!int.TryParse(textbox.Text, out i))
+                {
+                    message = " En este espacio solo se pueden colocar números: ";
+                    isOk = false;
+                }
+            }
+
+            if( textbox.Text == String.Empty)
+            {
+                isOk = false;
+                message += textBoxName;
+            }
+
+
+            if( !isOk)
+            {
+                Messagebox(message);
+            }
+
+            return isOk;
+        }
+
         protected void BtnAddStyle_Click(object sender, EventArgs e)
         {
+            if( checkField(TextBoxStyleAdd, false, "Estilo"))
+            {
+                SqlDataSourceStyle.InsertParameters["estilo"].DefaultValue = TextBoxStyleAdd.Text;
+                SqlDataSourceStyle.Insert();
+                Response.Redirect("Tablas_Atributos");
+                LabelConsole.Text = TextBoxStyleAdd.Text + " agregado en Estilos";
+            }
 
-            SqlDataSourceStyle.InsertParameters["estilo"].DefaultValue = TextBoxStyleAdd.Text;
-            SqlDataSourceStyle.Insert();
-            Response.Redirect("Tablas_Atributos");
-            LabelConsole.Text = TextBoxStyleAdd.Text + " agregado en Estilos";
 
         }
 
         protected void BtnAddColour_Click(object sender, EventArgs e)
         {
-            SqlDataSourceColour.InsertParameters["color"].DefaultValue = TextBoxColourAdd.Text;
-            SqlDataSourceColour.Insert();
-            Response.Redirect("Tablas_Atributos");
-            LabelConsole.Text = TextBoxColourAdd.Text + " agregado en Colores";
+            if (checkField(TextBoxColourAdd, false, "Color"))
+            {
+                SqlDataSourceColour.InsertParameters["color"].DefaultValue = TextBoxColourAdd.Text;
+                SqlDataSourceColour.Insert();
+                Response.Redirect("Tablas_Atributos");
+                LabelConsole.Text = TextBoxColourAdd.Text + " agregado en Colores";
+            }
 
         }
 
         protected void BtnAddSize_Click(object sender, EventArgs e)
         {
-            SqlDataSourceSize.InsertParameters["talla"].DefaultValue = TextBoxSizeAdd.Text;
-            SqlDataSourceSize.Insert();
-            Response.Redirect("Tablas_Atributos");
-            LabelConsole.Text = TextBoxSizeAdd.Text + " agregado en Tallas";
-
+            if (checkField(TextBoxStyleAdd, true, "Talla"))
+            {
+                SqlDataSourceSize.InsertParameters["talla"].DefaultValue = TextBoxSizeAdd.Text;
+                SqlDataSourceSize.Insert();
+                Response.Redirect("Tablas_Atributos");
+                LabelConsole.Text = TextBoxSizeAdd.Text + " agregado en Tallas";
+            }
         }
 
         protected void BtnAddLine_Click(object sender, EventArgs e)
         {
-            SqlDataSourceLine.InsertParameters["genero"].DefaultValue = TextBoxLineAdd.Text;
-            SqlDataSourceLine.Insert();
-            Response.Redirect("Tablas_Atributos");
-            LabelConsole.Text = TextBoxLineAdd.Text + " agregado en Lineas";
-
+            if( checkField(TextBoxLineAdd, false, "Linea") ){
+                SqlDataSourceLine.InsertParameters["genero"].DefaultValue = TextBoxLineAdd.Text;
+                SqlDataSourceLine.Insert();
+                Response.Redirect("Tablas_Atributos");
+                LabelConsole.Text = TextBoxLineAdd.Text + " agregado en Lineas";
+            }
         }
     }
 }

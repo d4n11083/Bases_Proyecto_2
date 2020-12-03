a3c7cc6631a1305abe83f09680f20fe7a97442db
Josué Chaves <d4n11083@estudiantec.cr>
2020-12-03 10:53:41 -0600

    Nuevo_Producto actualizada los dropdowns ya hacen fetch de los datos, e inserta en la tabla zapatos, falta validaciones de los campos vacios

--- Visual/Inventario Zapateria/Nuevo_Producto.aspx
+++ Visual/Inventario Zapateria/Nuevo_Producto.aspx
@@ -13,16 +13,46 @@
     <asp:Panel ID="Panel1" runat="server" updatemode="Always">
 
         <asp:Label ID="labelLine" runat="server" Text="Linea: "></asp:Label>
-        <asp:DropDownList ID="ddlLine"  runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlLine"  runat="server"  DataSourceID="SqlDataSourceline" DataTextField="genero" 
+            DataValueField="id_Linea" AppendDataBoundItems="true"></asp:DropDownList>
+
+        <asp:SqlDataSource ID="SqlDataSourceLine" runat="server" ConnectionString="<%$ ConnectionStrings:InventarioZapateriaConnectionStringJoshua %>" SelectCommand="LineasSelect_usp" SelectCommandType="StoredProcedure">
+            <SelectParameters>
+                <asp:Parameter DefaultValue="NULL" Name="id_Linea" Type="DBNull" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+
+
 
         <asp:Label ID="labelStyle" runat="server" Text="Estilo: "></asp:Label>
-        <asp:DropDownList ID="ddlStyle" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlStyle" runat="server" DataSourceID="SqlDataSourceStyle" DataTextField="estilo" DataValueField="id_Estilo"></asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSourceStyle" runat="server" ConnectionString="<%$ ConnectionStrings:InventarioZapateriaConnectionStringJoshua %>" SelectCommand="EstilosSelect_usp" SelectCommandType="StoredProcedure">
+            <SelectParameters>
+                <asp:Parameter DefaultValue="NULL" Name="id_Estilo" Type="DBNull" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+
+
 
         <asp:Label ID="labelColour" runat="server" Text="Color: "></asp:Label>
-        <asp:DropDownList ID="ddlColour" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlColour" runat="server" DataSourceID="SqlDataSourceColour" DataTextField="color" DataValueField="id_Color"></asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSourceColour" runat="server" ConnectionString="<%$ ConnectionStrings:InventarioZapateriaConnectionStringJoshua %>" SelectCommand="ColoresSelect_usp" SelectCommandType="StoredProcedure">
+            <SelectParameters>
+                <asp:Parameter DefaultValue="NULL" Name="id_Color" Type="DBNull" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+
+
 
         <asp:Label ID="labelSize" runat="server" Text="Talla: "></asp:Label>
-        <asp:DropDownList ID="ddlSize" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlSize" runat="server" DataSourceID="SqlDataSourceSize" DataTextField="talla" DataValueField="id_Talla"></asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSourceSize" runat="server" ConnectionString="<%$ ConnectionStrings:InventarioZapateriaConnectionStringJoshua %>" SelectCommand="TallasSelect_usp" SelectCommandType="StoredProcedure">
+            <SelectParameters>
+                <asp:Parameter DefaultValue="NULL" Name="id_Talla" Type="DBNull" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+
+
 
         <asp:Label ID="labelQuantity" runat="server" Text="Cantidad: "></asp:Label>
         <asp:TextBox ID="textBoxQuant" runat="server"></asp:TextBox>
@@ -36,6 +66,16 @@
     <asp:Button ID="AddButton" runat="server" OnClick="addShoe" OnClientClick="addShoe" Text="  +  " />
 
     <asp:Label ID="labelConsole" runat="server" ></asp:Label>
+    <asp:SqlDataSource ID="SqlDataSourceShoes" runat="server" ConnectionString="<%$ ConnectionStrings:InventarioZapateriaConnectionStringJoshua %>" InsertCommand="ZapatosInsert_usp" InsertCommandType="StoredProcedure" SelectCommand="ZapatosSelect_usp" SelectCommandType="StoredProcedure" >
+        <InsertParameters>
+            <asp:Parameter Name="modelo" Type="String" />
+            <asp:Parameter Name="descripcion" Type="String" />
+            <asp:Parameter Name="fk_color" Type="Int32" />
+            <asp:Parameter Name="fk_talla" Type="Int32" />
+            <asp:Parameter Name="fk_estilo" Type="Int32" />
+            <asp:Parameter Name="fk_linea" Type="Int32" />
+        </InsertParameters>
+        </asp:SqlDataSource>
     </div>
     
 
--- Visual/Inventario Zapateria/Nuevo_Producto.aspx.cs
+++ Visual/Inventario Zapateria/Nuevo_Producto.aspx.cs
@@ -13,13 +13,6 @@ namespace Inventario_Zapateria
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            List<DropDownList> dropDowns = Panel1.Controls.OfType<DropDownList>().ToList();
-
-            foreach (DropDownList ddl in dropDowns)
-            {
-                FillDropDownList(ddl, ddl.ID);
-            }
-
         }
 
         public void Messagebox(string xMessage)
@@ -27,93 +20,19 @@ namespace Inventario_Zapateria
             Response.Write("<script>alert('" + xMessage + "')</script>");
         }
 
-        private void FillDropDownList(DropDownList dropDown, string key)
-        {
-            List<string> lista = new List<string>();
-            lista.Add(key + "Hola");
-            lista.Add(key + "Hola2");
-            lista.Add(key + "Hola3");
-
-            foreach (string t in lista)
-            {
-                ListItem li = new ListItem();
-                li.Value = t;
-                li.Text = t;
-                dropDown.Items.Add(li);
-            }
-        }
-
-
-        private void CreateNewShoe(int id)
-        {
-            List<string> fieldNameList = new List<string>();
-            fieldNameList.Add("Linea");
-            fieldNameList.Add("Estilo");
-            fieldNameList.Add("Color");
-            fieldNameList.Add("Talla");
-
-            foreach (string fieldName in fieldNameList)
-            {
-                Label labelField = new Label();
-                labelField.ID = fieldName + id.ToString();
-                labelField.Text = fieldName + " : ";
-
-                DropDownList dropList = new DropDownList();
-                dropList.ID = fieldName + id.ToString();
-                FillDropDownList(dropList, fieldName);
-
-
-                Panel1.Controls.Add(labelField);
-                Panel1.Controls.Add(dropList);
-
-            }
-
-            Label labelQuantity = new Label();
-            labelQuantity.ID = "Cantidad" + id.ToString();
-            labelQuantity.Text = "Cantidad : ";
-
-            TextBox textBoxField = new TextBox();
-            textBoxField.ID = "Cantidad" + id.ToString();
-
-            Panel1.Controls.Add(labelQuantity);
-            Panel1.Controls.Add(textBoxField);
-
-        }
-
-        protected void SaveShoe(object sender, EventArgs e)
-        {
-            List<DropDownList> dropDowns = Panel1.Controls.OfType<DropDownList>().ToList();
-            List<TextBox> textBoxes = Panel1.Controls.OfType<TextBox>().ToList();
-
-            int total = textBoxes.Count;
-            string elements = "{ ";
-
-            for (int i = 0; i < total; i++)
-            {
-                string shoe = "Modelo: " + textBoxModelo.Text + " ,Descripcion: " + TextBoxDescripcion.Text;
-                for (int x = 0; i < 4; i++)
-                {
-                    shoe += " ," + dropDowns.First().ID + ": ";
-                    shoe += dropDowns.First().SelectedValue;
-                    dropDowns.RemoveAt(0);
-
-                }
-                shoe += " ,Cantidad: ";
-                shoe += textBoxes.First().Text;
-                textBoxes.RemoveAt(0);
-                shoe += "  }";
-                elements += shoe;
-
-            }
-
-            labelConsole.Text = elements;
-
-            Messagebox(elements);
-        }
-
         protected void addShoe(object sender, EventArgs e)
         {
-            labelConsole.Text = "AddShoe";
+            SqlDataSourceShoes.InsertParameters["modelo"].DefaultValue = textBoxModelo.Text;
+            SqlDataSourceShoes.InsertParameters["descripcion"].DefaultValue = TextBoxDescripcion.Text;
+            SqlDataSourceShoes.InsertParameters["fk_color"].DefaultValue = ddlColour.SelectedValue;
+            SqlDataSourceShoes.InsertParameters["fk_talla"].DefaultValue = ddlSize.SelectedValue;
+            SqlDataSourceShoes.InsertParameters["fk_estilo"].DefaultValue = ddlStyle.SelectedValue;
+            SqlDataSourceShoes.InsertParameters["fk_linea"].DefaultValue = ddlLine.SelectedValue;
+            SqlDataSourceShoes.Insert();
+
+            Messagebox("Elemento anadido");
+            Response.Redirect("Nuevo_Producto");
+
         }
 
 
